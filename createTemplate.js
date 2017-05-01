8ee3b20e1d67dc5ae80accc7cc2fe454ed9b5651
latanasov <lyuata@outlook.com>
2017-05-01 15:11:02 +0300

    format

--- createTemplate.js
+++ createTemplate.js
@@ -1,20 +1,20 @@
-var pub_key='[ENTER_HERE]';
-var priv_key='[ENTER_HERE]'
+var pub_key = '[ENTER_HERE]';
+var priv_key = '[ENTER_HERE]'
 var Mailjet = require('node-mailjet').connect(pub_key, priv_key);
 var sendEmail = Mailjet.post('template');
 var templateData = {
-  "Name":"Did it work no duplicate name +22!?"
+  "Name": "Did it work no duplicate name +22!?"
 }
 
-function handleError (err) {
+function handleError(err) {
   console.log(err.statusCode);
 }
 
-function handlePostResponse (result) {
-  var data=result.body.Data;
+function handlePostResponse(result) {
+  var data = result.body.Data;
   console.log(data);
 }
 sendEmail
-.request(templateData)
-.then(result => handlePostResponse(result))
-.catch(err=>handleError(err));
+  .request(templateData)
+  .then(result => handlePostResponse(result))
+  .catch(err => handleError(err));
